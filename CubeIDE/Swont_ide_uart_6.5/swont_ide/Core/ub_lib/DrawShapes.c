389a9fcc48b324b4dbc4bbfe0eb71bec0ce2cd24
Sjoerd den Breejen <sjoerd.denbreejen@gmail.com>
2023-05-10 13:48:56 +0200

    Function draw rectangle complete filled and unfilled

--- CubeIDE/Swont_ide_uart_6.5/swont_ide/Core/ub_lib/DrawShapes.c
+++ CubeIDE/Swont_ide_uart_6.5/swont_ide/Core/ub_lib/DrawShapes.c
@@ -8,25 +8,53 @@
 #include <main.h>
 
 /**
- * @fn int DrawRectangle(uint8_t, uint8_t, uint8_t, uint8_t, uint8_t)
+ * @fn int DrawRectangle(uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t)
  * @brief Draws a rectangle with the specified color within the x & y coordinates
  *
- * @param xp1
- * @param yp1
- * @param xp2
- * @param yp2
+ * @param xp
+ * @param yp
+ * @param Width
+ * @param Heigth
  * @param color
+ * @param filled
  * @return
  */
-int DrawRectangle(uint8_t xp1, uint8_t yp1, uint8_t xp2, uint8_t yp2, uint8_t color)
-{
-	 for(uint8_t y = yp1; y < yp2; y++)
-	 {
-		 for(uint8_t x = xp1; x < xp2; x++)
-		 {
-			 UB_VGA_SetPixel(x, y, color);
+int DrawRectangle(uint8_t xp, uint8_t yp, uint8_t Width, uint8_t Heigth,
+		uint8_t color, uint8_t filled) {
+	if (filled == 0)
+	{
+		for (uint8_t y = yp; y < yp + Heigth; y++)
+		{
+			for (uint8_t x = xp; x < xp + Width; x++)
+			{
+				UB_VGA_SetPixel(x, y, color);
+			}
+		}
+	}
+	//rectangle not filled only lines
+	if(filled == 1)
+	{
+		//top line
+		for (uint8_t x = xp; x < xp + Width; x++)
+		{
+			UB_VGA_SetPixel(x, yp, color);
+		}
+		//bottom line
+		for (uint8_t x = xp; x < xp + Width; x++)
+		{
+			UB_VGA_SetPixel(x, yp+Heigth, color);
+		}
 
-		 }
+		//Left vertical line
+		for (uint8_t y = yp; y < yp+Heigth; y++)
+		{
+			UB_VGA_SetPixel(xp, y, color);
 
-	 }
+		}
+		//Right vertical line
+		for (uint8_t y = yp; y < yp+Heigth; y++)
+		{
+			UB_VGA_SetPixel(xp+Width, y, color);
+		}
+	}
 }
