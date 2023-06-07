100bcc01f99a73139ac379e4d3b7fb11ec1df61e
Sjoerd den Breejen <sjoerd.denbreejen@gmail.com>
2023-06-07 14:02:25 +0200

    Added mainpage for doxygen i hope :s

---
+++ CubeIDE/Swont_ide_uart_6.5/swont_ide/Core/Src/mainpage.h
@@ -0,0 +1,24 @@
+/*
+ * mainpage.h
+ *
+ *  Created on: 7 jun. 2023
+ *      Author: Sjoerd
+ */
+
+#ifndef SRC_MAINPAGE_H_
+#define SRC_MAINPAGE_H_
+
+/** @file mainpage.h
+* @brief Definition of class Template
+*/
+
+/** @mainpage package templates
+*
+* @authors Ruben Middelman, Niels van der Zijden & Sjoerd den Breejen
+*
+*/
+/*! Functional design of the project:
+*  \image html images/Functioneel_Schema.png
+*/
+
+#endif /* SRC_MAINPAGE_H_ */
